93854b58a44f0b187326ea19928717d53b7d5891
maddin <admin@maddin.cc>
2014-01-20 00:18:09 +0100

    fix

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+if(process.env.hasOwnProperty('OPENSHIFT_DATA_DIR')) {
+    GLOBAL.DATA_DIR = process.env.OPENSHIFT_DATA_DIR;
+} else {
+    GLOBAL.DATA_DIR = __dirname + '/data/';
+}
 GLOBAL.GLaDOS = require('./lib/GLaDOS');
 require('fs').readdir("./plugins", function(err, files) {
     files.forEach(function(fileName) {
