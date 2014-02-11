d6e00aa636a1600904a1cd5a7746693a0208f4ec
maddin <admin@maddin.cc>
2014-02-11 20:38:30 +0100

    added custom logger

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 GLOBAL.VERSION = require('./package.json').version;
 GLOBAL.GLaDOS = require('./lib/GLaDOS');
-require('./lib/webserver');
 require('fs').readdir("./plugins", function(err, files) {
     files.forEach(function(fileName) {
         if(fileName === 'example.js') return;
