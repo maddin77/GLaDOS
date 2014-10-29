22929dda8133f54bcb1d346d886f85942378a98f
IMexiz <maxi.kaspers@aol.de>
2014-10-29 19:00:18 +0100

    Update henner.js

--- scripts/henner.js
+++ scripts/henner.js
@@ -14,6 +14,12 @@ module.exports = function (scriptLoader) {
         'TV Total Stock Car 2010 - ich war dabei', // http://forum.sa-mp.de/index.php?page=User&userID=23377
         'Soo ich bin der Henner alias Kai bin fast 33 und verheiratet und habe 2 Kinder', // http://sa-mp.de/B++/p1150653-/
         'wenn man brennt hat man pech dabei sein ist doch alles manche würden sich freuen das mal mit zuerleben', //http://sa-mp.de/B++/p1150721-/
+        'Ein Forum das Online ist ohne Impressum ist gefährlich', //http://forum.sa-mp.de/gta-fremdes/werbung-und-events/homepage-werbung/210872-die-deutsche-epoch-community/#post1985231
+        'ich finde es hier lustig ich bleibe', //http://forum.sa-mp.de/gta-fremdes/off-topic/smalltalk/208895-smalltalk-226#post1985116
+        'bin schon viel zu alt für die Samp Szene', //http://forum.sa-mp.de/gta-fremdes/off-topic/smalltalk/208895-smalltalk-225#post1985020
+        'man redet doch immer seine eigene Projekte gut', //http://forum.sa-mp.de/gta-fremdes/off-topic/smalltalk/208895-smalltalk-186#post1981636
+        'Naja sagen wir mal soo Ich habe da so ein Gefühl kenne je auch viele RP Server', //http://forum.sa-mp.de/gta-fremdes/off-topic/smalltalk/208895-smalltalk-180#post1981182
+        'Its henner Time', //sagt er zu oft für eine Quelle. Eine: https://www.youtube.com/watch?feature=player_detailpage&v=J_nzc-dpqAQ&list=UUsdf_os0T0MizL_GRDYsLDQ#t=173
     ];
 
     scriptLoader.on('command', 'henner', function (event) {
